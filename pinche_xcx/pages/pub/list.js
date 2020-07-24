053129f9a6a07a8bb2680d56b59174532b51a283
yangfei <fei.yang@joyoudata.com>
2020-07-24 18:02:05 +0800

    notice

--- pinche_xcx/pages/pub/list.js
+++ pinche_xcx/pages/pub/list.js
@@ -1,13 +1,42 @@
+var app = getApp();
 var util = require('../../utils/util.js');
+var page = 1;
+var list = new Array();
 Page({
   data:{},
-  onLoad:function(options){
+  onLoad:function(){
+    console.log("pub list onload")
     var that = this;
-    util.req('notice/list',{},function(data){
-      if(data.status == 1){
-        //消息通知
-        that.setData({data:data.data});
-      }
+    this.getList();
+  },
+  getList(){
+    var that = this;
+    util.req('info/mylist',{page:page},function(data){
+      if(data.data.length == 0){
+          if(page == 1){  
+            console.log(page)        ;
+            that.setData({'isnull':true});
+          }
+          that.setData({nomore:true});
+          return false;
+        } 
+
+      that.setData({list:data.data});
     })
-  }
+  },
+  onReachBottom:function(){
+    if(!this.data.nomore){
+      page++;
+      this.getList();
+    }
+  },
+  onPullDownRefresh: function(){
+    page = 1;
+    this.getList();
+    wx.stopPullDownRefresh();
+  },
+  // onShow:function(){
+  //   page = 1;
+  //   this.getList();
+  // }
 })
