ad180d25be51aceff3892bbdc64fcb623b344552
yangfei <fei.yang@joyoudata.com>
2020-07-27 14:27:49 +0800

    notice

--- pinche_xcx/pages/pub/index.js
+++ pinche_xcx/pages/pub/index.js
@@ -3,7 +3,7 @@ Page({
   data:{},
   onLoad:function(options){
     var that = this;
-    util.req('pub/detail',{id:options.id},function(data){
+    util.req('notice/detail',{id:options.id},function(data){
       if(data.code == 200){
         that.setData({data:data.data});
       }
